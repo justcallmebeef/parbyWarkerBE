134c0bce600ada26bb5187a4e3d6e9d0640fa303
justcallmebeef <bethanymitch10@gmail.com>
2019-01-08 15:25:48 -0700

    created put request and checked progress with Postman and updating glasses data works

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const express = require('express')
 const app = express() 
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const port = process.env.PORT || 3002
 
-const glasses = require('./data.json')
+app.use(bodyParser.json())
+app.use(cors())
+
+let glasses = require('./data.json')
 
 app.get('/', (req, res) => {
     res.json({ glasses })
 })
+
+app.get('/:id', (req, res, next) => {
+    let id = req.params.id
+    let glassesById = glasses.filter(glasses => {
+        return glasses.id == id
+    })
+    if(!glassesById.length) {
+        next()
+    }
+    res.send(glassesById)
+})
+
+app.put('/:id', (req, res, next) => {
+    let id = req.params.id
+    let body = req.body
+    let newGlassesArray = glasses.map(object => {
+        if(object.id == id) {
+            object = {
+                id: object.id, 
+                ... body
+            }
+        }
+        return object 
+    })
+    glasses = newGlassesArray
+    res.send({ glasses })
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
 
 app.listen(port)
